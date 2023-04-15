84d242aa00ebce6034834b62c6a788b39f2c2c91
A-Awad1 <eng.a.awad1@gmail.com>
2023-04-16 01:03:34 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,9 @@
 </template>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;700&display=swap");
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  font-family: "Kumbh Sans", sans-serif;
 }
 
 nav {
