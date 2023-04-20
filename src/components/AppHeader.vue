e6b3eab04be230bc0455e94347e1f912ccf73ab7
A-Awad1 <eng.a.awad1@gmail.com>
2023-04-20 16:32:08 +0200

    create cart list component

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav>
+  <nav id="main-nav">
     <div class="container">
       <img src="../assets/images/logo.svg" alt="logo" />
       <ul>
@@ -25,6 +25,7 @@
           width="22"
           height="20"
           xmlns="http://www.w3.org/2000/svg"
+          @click="CartList = !CartList"
         >
           <path
             d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z"
@@ -34,18 +35,30 @@
         </svg>
         <img src="../assets/images/image-avatar.png" alt="avatar" />
       </div>
+      <CartList />
+      <!-- <CartList v-if="CartList" /> -->
     </div>
   </nav>
 </template>
 
 <script>
+import CartList from "@/components/CartList.vue";
 export default {
   name: "AppHeader",
+  data: function () {
+    return {
+      CartList: false,
+    };
+  },
+  components: {
+    CartList,
+  },
 };
 </script>
 
 <style scoped lang="scss">
 nav {
+  position: relative;
   .container {
     > img {
       margin-top: -13px;
@@ -84,7 +97,7 @@ nav {
         cursor: pointer;
         &:hover {
           path {
-            fill: #000;
+            fill: $important-text-color;
           }
         }
       }
@@ -95,6 +108,7 @@ nav {
         &:hover {
           box-shadow: 0px 0px 0px 2px $orange-color;
         }
+        user-select: none;
       }
     }
   }
---
+++ src/components/CartList.vue
@@ -0,0 +1,72 @@
+<template>
+  <!-- <teleport to="#main-nav"> -->
+  <section>
+    <span>Cart</span>
+    <div>
+      <div>
+        <div>
+          <img
+            src="../assets/images/image-product-1-thumbnail.jpg"
+            alt="product-1"
+          />
+          <div>
+            <span>Fall Limited Edition Sneakers</span>
+            <span>$125.00 x 3 <span class="full-price">$375.00</span></span>
+          </div>
+          <img src="../assets/images/icon-delete.svg" alt="icon-delete" />
+        </div>
+      </div>
+      <button>Checkout</button>
+    </div>
+  </section>
+  <!-- </teleport> -->
+</template>
+
+<script>
+export default {
+  name: "CartList",
+};
+</script>
+
+<style scoped lang="scss">
+section {
+  position: absolute;
+  right: 0;
+  top: 100%;
+  border-radius: $main-border-radius;
+  box-shadow: 0px 0px 13px 1px $grayish-blue-color;
+  > span {
+    display: block;
+    font-weight: 700;
+    color: $important-text-color;
+    padding: $main-padding;
+    border-bottom: 1px solid $grayish-blue-color;
+  }
+  > div {
+    padding: $main-padding;
+    > div {
+      > div {
+        display: flex;
+        align-items: center;
+        gap: 10px;
+        img:first-of-type {
+          width: 40px;
+          border-radius: 4px;
+        }
+        div {
+          color: $main-text-color;
+          display: flex;
+          flex-direction: column;
+          gap: 3px;
+          > span > span {
+            font-weight: 700;
+            color: $important-text-color;
+          }
+        }
+      }
+    }
+    > button {
+    }
+  }
+}
+</style>
